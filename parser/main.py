96b2aeb75b048f690c1cc233d1f92e8226e172ac
Konrad <konrad.dumin.stud@pw.edu.pl>
2025-04-02 14:01:16 +0200

    model weights enumeration

--- parser/main.py
+++ parser/main.py
@@ -1,4 +1,8 @@
-
+from torchvision.models import resnet50, ResNet50_Weights
 
 if __name__ == '__main__':
-    pass
+    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+    # print(model)
+
+    for param in model.parameters():
+        print(param)
