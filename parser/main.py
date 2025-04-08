6f3ba70a2545f75bf27d1d98094501150d427191
Konrad <konrad.dumin.stud@pw.edu.pl>
2025-04-08 13:51:28 +0200

    Test of dumping model weights to a file

--- parser/main.py
+++ parser/main.py
@@ -4,5 +4,15 @@ if __name__ == '__main__':
     model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     # print(model)
 
+    params = []
     for param in model.parameters():
-        print(param)
+        # print(param)
+        params.append(param)
+
+    print(params)
+
+    with open('test.txt', 'w') as f:
+        for param in params:
+            for weight in param.data:
+                f.write(str(weight) + ',')
+            f.write('\n')
